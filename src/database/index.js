e49f5e077099650a42425886427323070426d41b
Nishith-Guptha <nishithguptha@gmail.com>
2022-02-17 10:32:09 +0530

    First Commit

---
+++ src/database/index.js
@@ -0,0 +1,9 @@
+const blogs = require('./blogs.json');
+const users = require('./users.json');
+// Something more
+
+module.exports = () => ({
+  users: users,
+  blogs: blogs
+  // Something more
+});
